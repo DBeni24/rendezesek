570aafa0bcf97118f83e010a6525061070cc7087
DBeni24 <d.benedek2004@gmail.com>
2025-03-18 08:23:29 +0100

    buborékos rendezés kísérlete

--- index.php
+++ index.php
@@ -1,1 +1,24 @@
+<?php
+$tomb=array(11, 5, 4, 8, 7, 9);
+$maxIndex=count($tomb)-1;
+print_r($tomb);
+echo "<br>";
 
+//&referencia 
+function csere(&$tomb, $j):void
+{
+    $b=$tomb[$j+1];
+    $tomb[$j+1]=$tomb[$j];
+    $tomb[$j]=$b;
+}
+for($i=$maxIndex;$i>=1;$j++)
+{
+    for($j=0;$j<=$i-1;$j++)
+    {
+        if($tomb[$j]>$tomb[$j+1])
+            {
+                csere($tomb, $j)
+            }
+    }
+}
+print_r($tomb);
